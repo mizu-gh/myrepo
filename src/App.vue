7482100300904c14a7073b3d34a6124b488a8ee9
mizu-gh <dummy@example.com>
2019-04-01 13:47:41 +0900

    p

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App5"/>
+    <HelloWorld msg="Welcome to Your Vue.js App51"/>
   </div>
 </template>
 
