a49632ef1f36d52a2f2e933b994c597fd75bd508
mizu-gh <dummy@example.com>
2019-11-12 18:00:14 +0900

    gg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      gFor a guide and recipes on how to configure / customize this project,<br>
+      ggggFor a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
